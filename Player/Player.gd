9650b916c5494d8c161d8e28eccdce66da11bdb1
tressc <tresscd@gmail.com>
2020-07-07 21:50:53 -0400

    Add player roll and refactor

--- Player/Player.gd
+++ Player/Player.gd
@@ -2,7 +2,8 @@ extends KinematicBody2D
 
 const ACCELERATION = 500
 const FRICTION = 500
-const MAX_SPEED = 100
+const MAX_SPEED = 80
+const ROLL_SPEED = 125
 
 enum {
 	MOVE,
@@ -12,6 +13,8 @@ enum {
 
 var state = MOVE
 var velocity = Vector2.ZERO
+var roll_vector = Vector2.LEFT
+
 
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
@@ -26,7 +29,7 @@ func _physics_process(delta):
 		MOVE:
 			move_state(delta)
 		ROLL:
-			pass
+			roll_state(delta)
 		ATTACK:
 			attack_state(delta)
 	
@@ -38,19 +41,35 @@ func move_state(delta):
 	input_vector = input_vector.normalized()
 
 	if input_vector != Vector2.ZERO:
+		roll_vector = input_vector
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
 		animationTree.set("parameters/Attack/blend_position", input_vector)
+		animationTree.set("parameters/Roll/blend_position", input_vector)
 		animationState.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
 		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
-	velocity = move_and_slide(velocity)
+	move()
 	
 	if Input.is_action_just_pressed("attack"):
 		state = ATTACK
+	elif Input.is_action_just_pressed("roll"):
+		state = ROLL
+	
+func roll_state(_delta):
+	velocity = roll_vector * ROLL_SPEED
+	animationState.travel("Roll")
+	move()
+	
+func roll_animation_finished():
+	velocity = velocity * 0.6
+	state = MOVE
+	
+func move():
+	velocity = move_and_slide(velocity)
 	
 func attack_state(_delta):
 	velocity = Vector2.ZERO
